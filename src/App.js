681c748162bb3e711b5e30bf6af115581775c957
jilla pavan <jillapavan22001@gmail.com>
2024-03-23 13:57:33 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { imgArray } from "./utils/constants";
+import img1 from "./images/1.png";
+import { useState } from "react";
 function App() {
+  const [activeImage, setActiveImage] = useState(0);
+  const clickHandlerNext = () => {
+    setActiveImage((activeImage + 1) % imgArray.length);
+  };
+  const clickHandlerPre = () => {
+    activeImage === 0
+      ? setActiveImage(imgArray.length - 1)
+      : setActiveImage(activeImage - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center mt-24">
+      <button
+        className="mx-2 border border-black py-2 rounded-full font-bold hover:bg-green-300 w-32"
+        onClick={() => {
+          clickHandlerPre();
+        }}
+      >
+        previous
+      </button>
+      <img
+        className="w-[700px] rounded-xl border border-black "
+        src={imgArray[activeImage]}
+      />
+      <button
+        className="mx-2 border border-black py-2 rounded-full font-bold hover:bg-green-300 w-32"
+        onClick={() => {
+          clickHandlerNext();
+        }}
+      >
+        next
+      </button>
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+
+import img1 from "../images/1.png";
+import img2 from "../images/2.png";
+import img3 from "../images/3.png";
+import img4 from "../images/4.png";
+
+export const imgArray = [img1, img2, img3, img4];
